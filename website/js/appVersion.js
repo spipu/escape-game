1ef0c45cc67387515cdbe8ecbb9b2896c76d3069
spipu <spipu45@gmail.com>
2024-04-16 17:43:31 +0200

    add new scenario

--- website/js/appVersion.js
+++ website/js/appVersion.js
@@ -2,7 +2,7 @@ class AppVersion {
     /** @type {int} */ currentVersion;
 
     constructor() {
-        this.currentVersion = 27;
+        this.currentVersion = 29;
         // this.currentVersion = (new Date()).getTime();
     }
 }
